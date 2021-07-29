80c7dcf5c0248f4f7007006c4ba8bbf31064a057
Mmorami <46481381+Mmorami@users.noreply.github.com>
2021-07-30 01:20:45 +0300

    problem B solved

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -1,29 +1,65 @@
-###########################
-# 6.0002 Problem Set 1b: Space Change
-# Name:
-# Collaborators:
-# Time:
-# Author: charz, cdenise
-
 #================================
 # Part B: Golden Eggs
 #================================
 
+
 # Problem 1
-def dp_make_weight(egg_weights, target_weight, memo = {}):
+# I chose to implement the function so that it keeps track on the actual eggs taken and the total weight only to see
+# the better result better. my previous solution returned the number of steps only and the code was slightly different.
+# The problem was that the last 2 tests i've made did not work since some branches included less steps but did not
+# maximize the gold value to be taken. in this implementation i force the algorithm to choose the highest amount of gold
+# even with a cost of taking extra steps.
+def dp_make_weight(egg_weights, target_weight, memo={}):
     """
     Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
     an infinite supply of eggs of each weight, and there is always a egg of value 1.
-    
+
     Parameters:
     egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
     target_weight - int, amount of weight we want to find eggs to fit
     memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
-    
+
     Returns: int, smallest number of eggs needed to make target weight
     """
-    # TODO: Your code here
-    pass
+    # first try to recall if this scenario has been spotted before. the key is a tuple with the list left to consider
+    # and the weight still available to pack
+    try:
+        return memo[(egg_weights, target_weight)]
+    # if the scenario never dealt with before compute
+    except KeyError:
+        # if the list of egg weights is empty or if there is no more space available return 0 and an empty list
+        if egg_weights == () or target_weight == 0:
+            result = (0, ())
+        # if the current item is too heavy to fit in the ship, remove the item from consideration and repeat the flow
+        elif egg_weights[-1] > target_weight:
+            result = dp_make_weight(egg_weights[:-1], target_weight, memo)
+        else:
+            # save the item to be taken in this node
+            next_item = egg_weights[-1]
+            # simulate taking an egg. when taking an egg it doesnt prevent us from taking another one of the same weight
+            with_weight, with_count = dp_make_weight(egg_weights, target_weight - next_item)
+            # here we count the total weight of the branch
+            with_weight += next_item
+            # simulate not taking an egg in this node, remove the possibility of taking an egg of this weight.
+            # this is because the order of taking is unimportant, so if we dont want to take it now, no reason why
+            # we would like to take it in the future.
+            without_weight, without_count = dp_make_weight(egg_weights[:-1], target_weight)
+            # if the branches result in same total weight we should check which one has less eggs
+            if with_weight == without_weight:
+                # takes the branch with the fewer eggs/steps that is not 0 eggs.
+                if len(with_count) > len(without_count) != len(()):
+                    result = (without_weight, without_count)
+                else:
+                    result = (with_weight, with_count + (next_item,))
+            # otherwise takes the branch that amounts to the higher weight
+            elif with_weight > without_weight:
+                result = (with_weight, with_count + (next_item,))
+            else:
+                result = (without_weight, without_count)
+            # record the specific situation in memp
+            memo[(egg_weights, target_weight)] = result
+        return result
+
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
@@ -32,5 +68,29 @@ if __name__ == '__main__':
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
-    print("Actual output:", dp_make_weight(egg_weights, n))
+    print("Actual output:", len(dp_make_weight(egg_weights, n)[1]))
+    print()
+
+    egg_weights = (1, 6, 14)
+    n = 19
+    print("Egg weights = (1, 6, 14)")
+    print("n = 19")
+    print("Expected ouput: 4 (3 * 6 + 1 * 1 = 19)")
+    print("Actual output:", len(dp_make_weight(egg_weights, n)[1]))
+    print()
+
+    egg_weights = (7, 11, 13)
+    n = 73
+    print("Egg weights = (7, 11, 13)")
+    print("n = 73")
+    print("Expected ouput: 7 (6 * 11 + 1 * 7 = 73) or 7 (4 * 13 + 3 * 7))")
+    print("Actual output:", len(dp_make_weight(egg_weights, n)[1]))
+    print()
+
+    egg_weights = (7, 11, 13)
+    n = 30
+    print("Egg weights = (7, 11, 13)")
+    print("n = 30")
+    print("Expected ouput: 3 (2 * 11 + 1 * 7 = 29)")
+    print("Actual output:", len(dp_make_weight(egg_weights, n)[1]))
     print()
--- tries.py
+++ tries.py
@@ -1,4 +1,93 @@
-dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-l = ['a', 'b', 'c', 'd']
-# l = [1,2,3,4]
-print(sum(dic[lambda i: l[i]]))
+# A very elegant and clever implementation of the algorithm by @John-L-Jones-IV i found on github.
+# notice that my last test makes the algorithm crash as it assumes that the limit can be reached exactly.
+
+def dp_make_weight(egg_weights, target_weight, memo={}):
+    """
+    Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
+    an infinite supply of eggs of each weight, and there is always a egg of value 1.
+
+    Parameters:
+    egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
+    target_weight - int, amount of weight we want to find eggs to fit
+    memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
+    NOTE: memo will keep memory of how it was built the first time it was used. It is possible to use the wrong memo on the set of
+    egg_weights. Or build a completly invalid memo when called again with a new set of egg_weights.
+
+    Returns: int, smallest number of eggs needed to make target weight
+    """
+    # memo's key is the target weight. if the target weight is not in memo check if it can be reached in 1 step
+    if target_weight not in memo:
+        if target_weight in egg_weights:
+            # save the fastest rout - 1 step
+            memo[target_weight] = 1
+        else:
+            #
+            memo[target_weight] = min(
+                # do the first line for each egg weight in the list
+                1 + dp_make_weight(egg_weights, target_weight - egg_weight, memo)
+                for egg_weight in egg_weights if target_weight > egg_weight
+            )
+
+    return memo.get(target_weight)
+
+
+# EXAMPLE TESTING CODE, feel free to add more if you'd like
+if __name__ == '__main__':
+    egg_weights = (1, 5, 10, 25)
+    n = 26
+    print("Egg weights = (1, 5, 10, 25)")
+    print("n = 26")
+    print("Expected ouput: 2 (25 + 1 = 26)")
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    n = 99
+    print("Egg weights = (1, 5, 10, 25)")
+    print("n = 99")
+    print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    n = 100
+    print("Egg weights = (1, 5, 10, 25)")
+    print("n = 100")
+    print("Expected ouput: 4 (4 * 25 = 100)")
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    n = 7
+    print("Egg weights = (1, 5, 10, 25)")
+    print("n = 7")
+    print("Expected ouput: 3 (1 * 5 + 2 * 1 = 100)")
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    n = 101
+    print("Egg weights = (1, 5, 10, 25)")
+    print("n = 101")
+    print("Expected ouput: 5 (4 * 25 + 1 * 1 = 100)")
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    egg_weights = (1, 5, 10, 20)
+    n = 99
+    print("Egg weights = ", egg_weights)
+    print("n = ", n)
+    print("Expected ouput: 10 (4 * 20 + 1 * 10 + 1 * 5 + 4 * 1 = 99)")
+    # print("Actual output:", dp_make_weight(egg_weights, n, {}))
+    print()
+
+    n = 100
+    print("Egg weights = ", egg_weights)
+    print("n = ", n)
+    print("Expected ouput: 5 (5 * 20 = 100)")
+    # print("Actual output:", dp_make_weight(egg_weights, n, {}))
+    print()
+
+    egg_weights = (7, 11, 13)
+    n = 30
+    print("Egg weights = (7, 11, 13)")
+    print("n = 30")
+    print("Expected ouput: 3 (2 * 11 + 1 * 7 = 29)")
+    print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
